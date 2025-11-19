aaac22759a7fd1eacc6cb28c6498955afe8209d5
Nikoloz Kavtaradze <nikoloz.k@onthegosystems.com>
2025-11-19 11:55:47 +0400

    Create the initial English file for translation

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import './App.css'
+import { useTranslation } from 'react-i18next';
 
 function App() {
+  const { t } = useTranslation();
+
   return (
     <div>
-      <h1>Welcome</h1>
-      <p>This is a localization demo.</p>
-      <button onClick={() => alert('Click me')}>Click me</button>
+      <h1>{t('welcome')}</h1>
+      <p>{t('description')}</p>
+      <button onClick={() => alert(t('clickMe'))}>{t('clickMe')}</button>
     </div>
   );
 }
 
-
-export default App
+export default App;
