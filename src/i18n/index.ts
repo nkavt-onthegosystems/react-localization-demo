22d02ec3c67cfd053d611721c6bdbf665b00b46e
Nikoloz Kavtaradze <nikoloz.k@onthegosystems.com>
2025-11-19 13:22:19 +0400

    Auto-Detecting User Language

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,12 +1,10 @@
-import i18n from 'i18next';
-import { initReactI18next } from 'react-i18next';
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
 
 type TranslationResources = string | { [k: string]: TranslationResources } | TranslationResources[];
 
-const modules = import.meta.glob('./locales/*.json', { eager: true }) as Record<
-  string,
-  { default: Record<string, TranslationResources> }
->;
+const modules = import.meta.glob<{default: Record<string, TranslationResources>}>('./locales/*.json', { eager: true })
 
 const resources: Record<string, { translation: Record<string, TranslationResources> }> = {};
 
@@ -18,11 +16,17 @@ for (const path in modules) {
 }
 
 i18n
+  .use(LanguageDetector) // detect browser/localStorage/etc.
   .use(initReactI18next)
   .init({
     resources,
-    fallbackLng: 'en',
+    fallbackLng: "en",
+    supportedLngs: ["en", "fr", "de", "ar"], // adjust to your languages
     interpolation: { escapeValue: false },
+    detection: {
+      order: ["querystring", "localStorage", "cookie", "navigator"],
+      caches: ["localStorage", "cookie"],
+    }
   });
 
 export default i18n;
