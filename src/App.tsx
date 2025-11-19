47422f9906fac272bb560bf0041dfc88a5244d8f
Nikoloz Kavtaradze <nikoloz.k@onthegosystems.com>
2025-11-19 12:56:33 +0400

    Add Plural Forms

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,27 @@
+import { useState } from 'react';
 import './App.css'
 import { useTranslation } from 'react-i18next';
 
 function App() {
   const { t } = useTranslation();
   const user = { firstName: 'Sarah' };
+  const [messageCount, setMessageCount] = useState<number>(1);
+
+  const handleIncrement = () => {
+    setMessageCount(messageCount + 1);
+  };
 
   return (
     <div>
       <h1>{t('welcome')}</h1>
       <p>{t('userGreeting', { firstName: user.firstName })}</p>
+      <p>{t('newMessages', { count: messageCount })}</p>
       <p>{t('description')}</p>
-      <button onClick={() => alert(t('clickMe'))}>{t('clickMe')}</button>
+      <button onClick={handleIncrement}>{t('clickMe')}</button>
     </div>
   );
 }
+
 
 export default App;
 
