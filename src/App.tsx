7ff0e6984fbddfb05e597e0c7b49339e4dd5614f
Nikoloz Kavtaradze <nikoloz.k@onthegosystems.com>
2025-11-19 13:15:15 +0400

    Trans Component: Adding Formatting and Links

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import { useState } from 'react';
 import './App.css'
-import { useTranslation } from 'react-i18next';
+import { Trans, useTranslation } from 'react-i18next';
 
 function App() {
   const { t } = useTranslation();
-  const user = { firstName: 'Sarah' };
+  const user = { firstName: "Sarah" };
   const [messageCount, setMessageCount] = useState<number>(1);
 
   const handleIncrement = () => {
@@ -13,14 +13,23 @@ function App() {
 
   return (
     <div>
-      <h1>{t('welcome')}</h1>
-      <p>{t('userGreeting', { firstName: user.firstName })}</p>
-      <p>{t('newMessages', { count: messageCount })}</p>
-      <p>{t('description')}</p>
-      <button onClick={handleIncrement}>{t('clickMe')}</button>
+      <h1>{t("welcome")}</h1>
+      <p>{t("userGreeting", { firstName: user.firstName })}</p>
+      <p>{t("newMessages", { count: messageCount })}</p>
+      <p>{t("description")}</p>
+
+      <p>
+        <Trans i18nKey="termsText">
+          I agree to the <a href="/terms">Terms of Service</a> and 
+          <a href="/privacy">Privacy Policy</a>.
+        </Trans>
+      </p>
+
+      <button onClick={handleIncrement}>{t("clickMe")}</button>
     </div>
   );
 }
+
 
 
 export default App;
